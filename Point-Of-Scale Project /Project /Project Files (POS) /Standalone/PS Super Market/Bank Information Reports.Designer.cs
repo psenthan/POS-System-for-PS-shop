9429c9177cfaa287266b77a3718c0b71e864195f
psenthan <senthanprasanth007@gmail.com>
2017-12-25 23:06:04 +0530

    Updated Program

---
+++ Point-Of-Scale Project /Project /Project Files (POS) /Standalone/PS Super Market/Bank Information Reports.Designer.cs
@@ -0,0 +1,123 @@
+﻿namespace PS_Super_Market
+{
+    partial class Bank_Information_Reports
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtbank = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // txtbank
+            // 
+            this.txtbank.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.SuggestAppend;
+            this.txtbank.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.ListItems;
+            this.txtbank.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtbank.FormattingEnabled = true;
+            this.txtbank.Items.AddRange(new object[] {
+            ""});
+            this.txtbank.Location = new System.Drawing.Point(132, 98);
+            this.txtbank.Name = "txtbank";
+            this.txtbank.Size = new System.Drawing.Size(267, 33);
+            this.txtbank.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(181, 137);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(184, 39);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "View Information";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(128, 75);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(167, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Choose Bank Name";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.BlueViolet;
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(518, 42);
+            this.panel1.TabIndex = 78;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial Narrow", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(144, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(234, 25);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Bank Account Information";
+            // 
+            // Bank_Information_Reports
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(518, 219);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtbank);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Bank_Information_Reports";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Load += new System.EventHandler(this.Bank_Information_Reports_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox txtbank;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label2;
+    }
+}
---
+++ Point-Of-Scale Project /Project /Project Files (POS) /Standalone/PS Super Market/Bank Information Reports.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using CrystalDecisions.CrystalReports.Engine;
+using CrystalDecisions.Shared;
+using System.Diagnostics;
+using System.IO;
+namespace PS_Super_Market
+{
+    public partial class Bank_Information_Reports : Form
+    {
+        public Bank_Information_Reports()
+        {
+            InitializeComponent();
+        }
+        MainClass db = new MainClass();
+        DataTable tb;
+        string query;
+        private void Bank_Information_Reports_Load(object sender, EventArgs e)
+        {
+            query = "select BankName from BankInformation group by BankName";
+            tb = db.Search(query);
+            if (tb.Rows.Count>0)
+            {
+                for (int i = 0; i < tb.Rows.Count; i++)
+                {
+                    txtbank.Items.Add(tb.Rows[i][0].ToString());
+                }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                this.Cursor = Cursors.WaitCursor;
+                ReportDocument rep = new ReportDocument();
+                rep.Load(@"C:\PS Supermarket\Bank Information.rpt");
+                string path = @"C:\PS Supermarket\Bank Information.pdf";
+                db.CrystalReportServer(rep);
+                rep.SetParameterValue("BankName", txtbank.Text);
+                if (File.Exists(path))
+                {
+                    File.Delete(path);
+                }
+                rep.ExportToDisk(ExportFormatType.PortableDocFormat, path);
+                Process.Start(path);
+                this.Cursor = Cursors.Default;
+            }
+            catch (Exception ex)
+            {
+                this.Cursor = Cursors.Default;
+                MessageBox.Show(ex.Message.ToString(), "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
