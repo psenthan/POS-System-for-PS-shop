aa08ae02acfc4cb8130da898c28692dd8f4cc3a4
psenthan <senthanprasanth007@gmail.com>
2017-12-26 20:38:34 +0530

    Form implementation

---
+++ Point-Of-Scale Project /Project /Project Files (POS) /Standalone/PS Super Market/Form1.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace PS_Super_Market
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.txtusername = new MaterialSkin.Controls.MaterialSingleLineTextField();
+            this.txtpassword = new MaterialSkin.Controls.MaterialSingleLineTextField();
+            this.materialRaisedButton1 = new MaterialSkin.Controls.MaterialRaisedButton();
+            this.txtdate = new System.Windows.Forms.DateTimePicker();
+            this.SuspendLayout();
+            // 
+            // txtusername
+            // 
+            this.txtusername.BackColor = System.Drawing.Color.White;
+            this.txtusername.Depth = 0;
+            this.txtusername.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtusername.Hint = "";
+            this.txtusername.Location = new System.Drawing.Point(431, 175);
+            this.txtusername.MaxLength = 32767;
+            this.txtusername.MouseState = MaterialSkin.MouseState.HOVER;
+            this.txtusername.Name = "txtusername";
+            this.txtusername.PasswordChar = '\0';
+            this.txtusername.SelectedText = "";
+            this.txtusername.SelectionLength = 0;
+            this.txtusername.SelectionStart = 0;
+            this.txtusername.Size = new System.Drawing.Size(337, 23);
+            this.txtusername.TabIndex = 0;
+            this.txtusername.TabStop = false;
+            this.txtusername.UseSystemPasswordChar = false;
+            // 
+            // txtpassword
+            // 
+            this.txtpassword.BackColor = System.Drawing.Color.White;
+            this.txtpassword.Depth = 0;
+            this.txtpassword.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtpassword.Hint = "";
+            this.txtpassword.Location = new System.Drawing.Point(431, 231);
+            this.txtpassword.MaxLength = 32767;
+            this.txtpassword.MouseState = MaterialSkin.MouseState.HOVER;
+            this.txtpassword.Name = "txtpassword";
+            this.txtpassword.PasswordChar = '\0';
+            this.txtpassword.SelectedText = "";
+            this.txtpassword.SelectionLength = 0;
+            this.txtpassword.SelectionStart = 0;
+            this.txtpassword.Size = new System.Drawing.Size(337, 23);
+            this.txtpassword.TabIndex = 1;
+            this.txtpassword.TabStop = false;
+            this.txtpassword.UseSystemPasswordChar = true;
+            this.txtpassword.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtpassword_KeyPress);
+            // 
+            // materialRaisedButton1
+            // 
+            this.materialRaisedButton1.AutoSize = true;
+            this.materialRaisedButton1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.materialRaisedButton1.Depth = 0;
+            this.materialRaisedButton1.Icon = null;
+            this.materialRaisedButton1.Location = new System.Drawing.Point(537, 260);
+            this.materialRaisedButton1.MouseState = MaterialSkin.MouseState.HOVER;
+            this.materialRaisedButton1.Name = "materialRaisedButton1";
+            this.materialRaisedButton1.Primary = true;
+            this.materialRaisedButton1.Size = new System.Drawing.Size(139, 36);
+            this.materialRaisedButton1.TabIndex = 2;
+            this.materialRaisedButton1.Text = "Login to System";
+            this.materialRaisedButton1.UseVisualStyleBackColor = true;
+            this.materialRaisedButton1.Click += new System.EventHandler(this.materialRaisedButton1_Click);
+            // 
+            // txtdate
+            // 
+            this.txtdate.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.txtdate.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.txtdate.Location = new System.Drawing.Point(653, 149);
+            this.txtdate.Name = "txtdate";
+            this.txtdate.Size = new System.Drawing.Size(115, 20);
+            this.txtdate.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(812, 421);
+            this.Controls.Add(this.txtdate);
+            this.Controls.Add(this.materialRaisedButton1);
+            this.Controls.Add(this.txtpassword);
+            this.Controls.Add(this.txtusername);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private MaterialSkin.Controls.MaterialSingleLineTextField txtusername;
+        private MaterialSkin.Controls.MaterialSingleLineTextField txtpassword;
+        private MaterialSkin.Controls.MaterialRaisedButton materialRaisedButton1;
+        private System.Windows.Forms.DateTimePicker txtdate;
+    }
+}
+
---
+++ Point-Of-Scale Project /Project /Project Files (POS) /Standalone/PS Super Market/Form1.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PS_Super_Market
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        string query, msg;
+        MainClass db = new MainClass();
+        DataTable tb;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //calling a method. which checks for existing users, if no any, system suggest to register a user with admin privilage.
+            CheckExistingUsers();
+        }
+
+        private void CheckExistingUsers()
+        {
+            //query to get records where role is Admin. 
+            query = "select * from Login where Role ='"+db.Encrypt("Administrator")+"'";
+            tb = db.Search(query);
+            if (tb.Rows.Count > 0)
+            {
+                //if any user exist. do nothing
+            }
+            else
+            {
+                //no user found. promote to register a user.
+                User_Management ur = new User_Management();
+                ur.ShowDialog();
+                CheckExistingUsers();
+            }
+        }
+
+        private void materialRaisedButton1_Click(object sender, EventArgs e)
+        {
+            Authenticate();
+        }
+
+        private void txtpassword_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar==(char)Keys.Enter)
+            {
+                Authenticate();
+            }
+        }
+
+        private void Authenticate()
+        {
+            if (txtusername.Text != "" && txtpassword.Text != "")
+            {
+                query = "select * from Shift where Date<>'" + txtdate.Text + "' and Status<>'" + "Closed" + "'";
+                tb = db.Search(query);
+                if (tb.Rows.Count > 0)
+                {
+                    Cashdraw_Close cr = new Cashdraw_Close();
+                    cr.Date = tb.Rows[0][0].ToString();
+                    cr.ShowDialog();
+                    Authenticate();
+                }
+                else
+                {
+                    query = "select Active, Role from Login where Username='" + db.Encrypt(txtusername.Text) + "' and Password='" + db.Encrypt(txtpassword.Text) + "' and Active='" + db.Encrypt("1") + "'";
+                    tb = db.Search(query);
+                    if (tb.Rows.Count > 0)
+                    {
+                        Main_Menu mm = new Main_Menu();
+                        string[] info = { txtdate.Text, txtusername.Text };
+                        mm.Info = info;
+                        mm.Show();
+                        this.Hide();
+                    }
+                    else
+                    {
+                        MessageBox.Show("Invalid Entry", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+            }
+            else
+            {
+                MessageBox.Show("Invalid Entry", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
