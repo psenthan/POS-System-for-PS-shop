8130b638498a23995784dd3d397cce82ce845f2f
psenthan <senthanprasanth007@gmail.com>
2017-12-27 21:57:22 +0530

    Chart implementation

---
+++ Point-Of-Scale Project /Project /Project Files (POS) /Standalone/PS Super Market/Sales Chart Reports.Designer.cs
@@ -0,0 +1,126 @@
+﻿namespace PS_Super_Market
+{
+    partial class Sales_Chart_Reports
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtyear = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.materialFlatButton1 = new MaterialSkin.Controls.MaterialFlatButton();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel1.BackColor = System.Drawing.Color.BlueViolet;
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(-10, -4);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(562, 50);
+            this.panel1.TabIndex = 71;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial Narrow", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(171, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(212, 25);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Sales Chart Information";
+            // 
+            // txtyear
+            // 
+            this.txtyear.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtyear.FormattingEnabled = true;
+            this.txtyear.Location = new System.Drawing.Point(166, 129);
+            this.txtyear.Name = "txtyear";
+            this.txtyear.Size = new System.Drawing.Size(230, 33);
+            this.txtyear.TabIndex = 72;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(162, 106);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(103, 20);
+            this.label2.TabIndex = 73;
+            this.label2.Text = "Select Year";
+            // 
+            // materialFlatButton1
+            // 
+            this.materialFlatButton1.AutoSize = true;
+            this.materialFlatButton1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.materialFlatButton1.Depth = 0;
+            this.materialFlatButton1.Icon = null;
+            this.materialFlatButton1.Location = new System.Drawing.Point(211, 171);
+            this.materialFlatButton1.Margin = new System.Windows.Forms.Padding(4, 6, 4, 6);
+            this.materialFlatButton1.MouseState = MaterialSkin.MouseState.HOVER;
+            this.materialFlatButton1.Name = "materialFlatButton1";
+            this.materialFlatButton1.Primary = false;
+            this.materialFlatButton1.Size = new System.Drawing.Size(144, 36);
+            this.materialFlatButton1.TabIndex = 74;
+            this.materialFlatButton1.Text = "Generate Report";
+            this.materialFlatButton1.UseVisualStyleBackColor = true;
+            this.materialFlatButton1.Click += new System.EventHandler(this.materialFlatButton1_Click);
+            // 
+            // Sales_Chart_Reports
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(537, 243);
+            this.Controls.Add(this.materialFlatButton1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtyear);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Sales_Chart_Reports";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Load += new System.EventHandler(this.Sales_Chart_Reports_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox txtyear;
+        private System.Windows.Forms.Label label2;
+        private MaterialSkin.Controls.MaterialFlatButton materialFlatButton1;
+    }
+}
---
+++ Point-Of-Scale Project /Project /Project Files (POS) /Standalone/PS Super Market/Sales Chart Reports.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using CrystalDecisions.CrystalReports.Engine;
+using CrystalDecisions.Shared;
+using System.Diagnostics;
+using System.IO;
+namespace PS_Super_Market
+{
+    public partial class Sales_Chart_Reports : Form
+    {
+        public Sales_Chart_Reports()
+        {
+            InitializeComponent();
+        }
+        string query, msg;
+        MainClass db = new MainClass();
+        DataTable tb;
+
+        private void Sales_Chart_Reports_Load(object sender, EventArgs e)
+        {
+            query = "select Year(Date) from Billing group by Year(Date)";
+            tb = db.Search(query);
+            if (tb.Rows.Count > 0)
+            {
+                for (int i = 0; i < tb.Rows.Count; i++)
+                {
+                    txtyear.Items.Add(tb.Rows[i][0].ToString());
+                }
+            }
+        }
+
+        private void materialFlatButton1_Click(object sender, EventArgs e)
+        {
+            if (txtyear.Text != "")
+            {
+                try
+                {
+                    db.Delete("delete from TempChart");
+                    query = "select Month(Date),sum(SubTotal) from Billing where Year(Date)='" + txtyear.Text + "' and Type='"+"F"+"' group by Month(Date)";
+                    tb = db.Search(query);
+                    if (tb.Rows.Count > 0)
+                    {
+                        for (int i = 0; i < tb.Rows.Count; i++)
+                        {
+                            query = "insert into TempChart values ('" + tb.Rows[i][0].ToString() + "', '" + tb.Rows[i][1].ToString() + "')";
+                            msg = db.Insert(query);
+                        }
+
+                        ReportDocument rep = new ReportDocument();
+                        rep.Load(@"C:\PS Supermarket\Sales Chart.rpt");
+                        string path = @"C:\PS Supermarket\Sales Chart.pdf";
+                        rep.SetParameterValue("Year", txtyear.Text);
+                        if (File.Exists(path))
+                        {
+                            File.Delete(path);
+                        }
+                        rep.ExportToDisk(ExportFormatType.PortableDocFormat, path);
+                        Process.Start(path);
+                    }
+                    else
+                    {
+                        MessageBox.Show("--No Records Found--", "Invalid Year", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+                    }
+                }
+                catch (Exception ex)
+                {
+
+                    MessageBox.Show(ex.Message.ToString(), "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            else
+            {
+                MessageBox.Show("--Invalid Entry--", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
