51908f4ecb76a044d3057ee76d38a284748d3522
psenthan <senthanprasanth007@gmail.com>
2017-12-28 21:32:35 +0530

    Purchasing information implementation

---
+++ Point-Of-Scale Project /Project /Project Files (POS) /Standalone/PS Super Market/Purchasing Information Reports.Designer.cs
@@ -0,0 +1,176 @@
+﻿namespace PS_Super_Market
+{
+    partial class Purchasing_Information_Reports
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.materialFlatButton1 = new MaterialSkin.Controls.MaterialFlatButton();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtto = new System.Windows.Forms.DateTimePicker();
+            this.txtfrom = new System.Windows.Forms.DateTimePicker();
+            this.label8 = new System.Windows.Forms.Label();
+            this.txtcustomer = new System.Windows.Forms.ComboBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // materialFlatButton1
+            // 
+            this.materialFlatButton1.AutoSize = true;
+            this.materialFlatButton1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.materialFlatButton1.Depth = 0;
+            this.materialFlatButton1.Icon = null;
+            this.materialFlatButton1.Location = new System.Drawing.Point(156, 185);
+            this.materialFlatButton1.Margin = new System.Windows.Forms.Padding(4, 6, 4, 6);
+            this.materialFlatButton1.MouseState = MaterialSkin.MouseState.HOVER;
+            this.materialFlatButton1.Name = "materialFlatButton1";
+            this.materialFlatButton1.Primary = false;
+            this.materialFlatButton1.Size = new System.Drawing.Size(108, 36);
+            this.materialFlatButton1.TabIndex = 48;
+            this.materialFlatButton1.Text = "View Report";
+            this.materialFlatButton1.UseVisualStyleBackColor = true;
+            this.materialFlatButton1.Click += new System.EventHandler(this.materialFlatButton1_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(220, 140);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(77, 13);
+            this.label4.TabIndex = 46;
+            this.label4.Text = "Ending Date";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(79, 140);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(82, 13);
+            this.label3.TabIndex = 47;
+            this.label3.Text = "Starting Date";
+            // 
+            // txtto
+            // 
+            this.txtto.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.txtto.Location = new System.Drawing.Point(223, 156);
+            this.txtto.Name = "txtto";
+            this.txtto.Size = new System.Drawing.Size(135, 20);
+            this.txtto.TabIndex = 45;
+            // 
+            // txtfrom
+            // 
+            this.txtfrom.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.txtfrom.Location = new System.Drawing.Point(82, 156);
+            this.txtfrom.Name = "txtfrom";
+            this.txtfrom.Size = new System.Drawing.Size(135, 20);
+            this.txtfrom.TabIndex = 44;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.ForeColor = System.Drawing.Color.Black;
+            this.label8.Location = new System.Drawing.Point(78, 86);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(126, 20);
+            this.label8.TabIndex = 50;
+            this.label8.Text = "Supplier Name";
+            // 
+            // txtcustomer
+            // 
+            this.txtcustomer.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.SuggestAppend;
+            this.txtcustomer.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.ListItems;
+            this.txtcustomer.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtcustomer.FormattingEnabled = true;
+            this.txtcustomer.Location = new System.Drawing.Point(82, 109);
+            this.txtcustomer.Name = "txtcustomer";
+            this.txtcustomer.Size = new System.Drawing.Size(276, 28);
+            this.txtcustomer.TabIndex = 49;
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel1.BackColor = System.Drawing.Color.BlueViolet;
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(-72, -7);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(562, 50);
+            this.panel1.TabIndex = 73;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial Narrow", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(136, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(274, 25);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Purchasing Information Report";
+            // 
+            // Purchasing_Information_Reports
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(419, 265);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.txtcustomer);
+            this.Controls.Add(this.materialFlatButton1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtto);
+            this.Controls.Add(this.txtfrom);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Purchasing_Information_Reports";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Load += new System.EventHandler(this.Purchasing_Information_Reports_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private MaterialSkin.Controls.MaterialFlatButton materialFlatButton1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.DateTimePicker txtto;
+        private System.Windows.Forms.DateTimePicker txtfrom;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.ComboBox txtcustomer;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+    }
+}
---
+++ Point-Of-Scale Project /Project /Project Files (POS) /Standalone/PS Super Market/Purchasing Information Reports.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using CrystalDecisions.CrystalReports.Engine;
+using CrystalDecisions.Shared;
+using System.IO;
+using System.Diagnostics;
+namespace PS_Super_Market
+{
+    public partial class Purchasing_Information_Reports : Form
+    {
+        public Purchasing_Information_Reports()
+        {
+            InitializeComponent();
+        }
+        DataTable tb;
+        MainClass db = new MainClass();
+        string query; 
+        private void Purchasing_Information_Reports_Load(object sender, EventArgs e)
+        {
+            LoadSuppliers();
+        }
+        private void LoadSuppliers()
+        {
+            query = "select Name from SupplierInformation";
+            tb = db.Search(query);
+            if (tb.Rows.Count > 0)
+            {
+                for (int i = 0; i < tb.Rows.Count; i++)
+                {
+                    if (!txtcustomer.Items.Contains(tb.Rows[i][0].ToString()))
+                    {
+                        txtcustomer.Items.Add(tb.Rows[i][0].ToString());
+                    }
+                }
+            }
+        }
+
+        private void materialFlatButton1_Click(object sender, EventArgs e)
+        {
+            this.Cursor = Cursors.WaitCursor;
+            try
+            {
+                ReportDocument rep = new ReportDocument();
+                rep.Load(@"C:\PS Supermarket\Purchasing Report.rpt");
+                string path = @"C:\PS Supermarket\Purchasing Information.pdf";
+                rep.SetParameterValue("From", txtfrom.Text);
+                rep.SetParameterValue("To", txtto.Text);
+                rep.SetParameterValue("Supplier", txtcustomer.Text);
+                if (File.Exists(path))
+                {
+                    File.Delete(path);
+                }
+                rep.ExportToDisk(ExportFormatType.PortableDocFormat, path);
+                Process.Start(path);
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message.ToString(), "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            this.Cursor = Cursors.Default;
+        }
+    }
+}
