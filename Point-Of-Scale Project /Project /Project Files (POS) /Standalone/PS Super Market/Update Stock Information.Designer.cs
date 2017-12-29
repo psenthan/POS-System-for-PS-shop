a53ff6c8ffa243fd3283329fc23274a2a9dda1d9
psenthan <senthanprasanth007@gmail.com>
2017-12-29 20:51:14 +0530

    Update stock information

---
+++ Point-Of-Scale Project /Project /Project Files (POS) /Standalone/PS Super Market/Update Stock Information.Designer.cs
@@ -0,0 +1,287 @@
+﻿namespace PS_Super_Market
+{
+    partial class Update_Stock_Information
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtitemcode = new System.Windows.Forms.TextBox();
+            this.txtdesciption = new System.Windows.Forms.TextBox();
+            this.txtcost = new System.Windows.Forms.TextBox();
+            this.txtsell = new System.Windows.Forms.TextBox();
+            this.txtalert = new System.Windows.Forms.TextBox();
+            this.txtcategory = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.lblitem = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.label7 = new System.Windows.Forms.Label();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.panel3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // txtitemcode
+            // 
+            this.txtitemcode.Location = new System.Drawing.Point(182, 80);
+            this.txtitemcode.Name = "txtitemcode";
+            this.txtitemcode.Size = new System.Drawing.Size(227, 20);
+            this.txtitemcode.TabIndex = 0;
+            // 
+            // txtdesciption
+            // 
+            this.txtdesciption.Location = new System.Drawing.Point(182, 106);
+            this.txtdesciption.Multiline = true;
+            this.txtdesciption.Name = "txtdesciption";
+            this.txtdesciption.Size = new System.Drawing.Size(227, 55);
+            this.txtdesciption.TabIndex = 1;
+            // 
+            // txtcost
+            // 
+            this.txtcost.Location = new System.Drawing.Point(182, 167);
+            this.txtcost.Name = "txtcost";
+            this.txtcost.Size = new System.Drawing.Size(87, 20);
+            this.txtcost.TabIndex = 2;
+            // 
+            // txtsell
+            // 
+            this.txtsell.Location = new System.Drawing.Point(182, 193);
+            this.txtsell.Name = "txtsell";
+            this.txtsell.Size = new System.Drawing.Size(87, 20);
+            this.txtsell.TabIndex = 3;
+            // 
+            // txtalert
+            // 
+            this.txtalert.Location = new System.Drawing.Point(343, 167);
+            this.txtalert.Name = "txtalert";
+            this.txtalert.Size = new System.Drawing.Size(66, 20);
+            this.txtalert.TabIndex = 4;
+            // 
+            // txtcategory
+            // 
+            this.txtcategory.FormattingEnabled = true;
+            this.txtcategory.Location = new System.Drawing.Point(182, 219);
+            this.txtcategory.Name = "txtcategory";
+            this.txtcategory.Size = new System.Drawing.Size(227, 21);
+            this.txtcategory.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(112, 80);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(64, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Item Code";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(105, 106);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(71, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Description";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(111, 167);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(65, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Cost Price";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(115, 193);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(61, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Sell Price";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(304, 167);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(33, 13);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "Alert";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(119, 222);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(57, 13);
+            this.label6.TabIndex = 6;
+            this.label6.Text = "Category";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(233, 258);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(132, 34);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Update Info";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // lblitem
+            // 
+            this.lblitem.AutoSize = true;
+            this.lblitem.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.lblitem.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblitem.ForeColor = System.Drawing.Color.Red;
+            this.lblitem.Location = new System.Drawing.Point(12, 48);
+            this.lblitem.Name = "lblitem";
+            this.lblitem.Size = new System.Drawing.Size(175, 13);
+            this.lblitem.TabIndex = 6;
+            this.lblitem.Text = "--Click Here To Choose Item--";
+            this.lblitem.Click += new System.EventHandler(this.lblitem_Click);
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.BlueViolet;
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Right;
+            this.panel2.Location = new System.Drawing.Point(534, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(10, 329);
+            this.panel2.TabIndex = 124;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.BlueViolet;
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel1.Location = new System.Drawing.Point(0, 317);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(534, 12);
+            this.panel1.TabIndex = 125;
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.BlueViolet;
+            this.panel3.Controls.Add(this.label7);
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel3.Location = new System.Drawing.Point(0, 0);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(534, 43);
+            this.panel3.TabIndex = 126;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Arial Narrow", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.ForeColor = System.Drawing.Color.White;
+            this.label7.Location = new System.Drawing.Point(148, 9);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(261, 25);
+            this.label7.TabIndex = 3;
+            this.label7.Text = "Update Inventory Information";
+            this.label7.Click += new System.EventHandler(this.label7_Click);
+            // 
+            // panel4
+            // 
+            this.panel4.BackColor = System.Drawing.Color.BlueViolet;
+            this.panel4.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel4.Location = new System.Drawing.Point(0, 43);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(10, 274);
+            this.panel4.TabIndex = 127;
+            // 
+            // Update_Stock_Information
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(544, 329);
+            this.Controls.Add(this.panel4);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.lblitem);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtcategory);
+            this.Controls.Add(this.txtalert);
+            this.Controls.Add(this.txtsell);
+            this.Controls.Add(this.txtcost);
+            this.Controls.Add(this.txtdesciption);
+            this.Controls.Add(this.txtitemcode);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Name = "Update_Stock_Information";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Load += new System.EventHandler(this.Update_Stock_Information_Load);
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtitemcode;
+        private System.Windows.Forms.TextBox txtdesciption;
+        private System.Windows.Forms.TextBox txtcost;
+        private System.Windows.Forms.TextBox txtsell;
+        private System.Windows.Forms.TextBox txtalert;
+        private System.Windows.Forms.ComboBox txtcategory;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label lblitem;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Panel panel4;
+    }
+}
---
+++ Point-Of-Scale Project /Project /Project Files (POS) /Standalone/PS Super Market/Update Stock Information.cs
@@ -0,0 +1,181 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PS_Super_Market
+{
+    public partial class Update_Stock_Information : Form
+    {
+        public Update_Stock_Information()
+        {
+            InitializeComponent();
+        }
+        DataTable tb;
+        MainClass db = new MainClass();
+        string query, msg;
+        private void Update_Stock_Information_Load(object sender, EventArgs e)
+        {
+            LoadCategory();
+            Choose();
+        }
+        string stockid="";
+        private void lblitem_Click(object sender, EventArgs e)
+        {
+            Choose();
+        }
+
+        private void Choose()
+        {
+            stockid = "";
+            Clipboard.Clear();
+            Find_Products fr = new Find_Products();
+            fr.ShowDialog();
+
+            if (Clipboard.GetText() != "")
+            {
+                stockid = Clipboard.GetText();
+                query = "select Inventory.ItemCode, ItemInformation.Description, Inventory.Cost, Inventory.SellPrice, Inventory.Alert, Inventory.Category, Inventory.ID from Inventory, ItemInformation where ItemInformation.ItemCode=Inventory.ItemCode and Inventory.ItemCode='" + stockid + "'";
+                tb = db.Search(query);
+                if (tb.Rows.Count > 0)
+                {
+                    stockid = tb.Rows[0][6].ToString();
+                    txtitemcode.Text = tb.Rows[0][0].ToString();
+                    txtdesciption.Text = tb.Rows[0][1].ToString();
+                    txtalert.Text = tb.Rows[0][4].ToString();
+                    txtcategory.Text = tb.Rows[0][5].ToString();
+                    if (tb.Rows.Count > 1)
+                    {
+                        Clipboard.Clear();
+                        Choose_Items cr = new Choose_Items();
+                        cr.ItemCode = txtitemcode.Text;
+                        cr.ShowDialog();
+                        if (Clipboard.GetText() != "")
+                        {
+                            
+                            query = "select Cost, SellPrice,ID from Inventory where ID='" + Clipboard.GetText() + "'";
+                            tb = db.Search(query);
+                            if (tb.Rows.Count > 0)
+                            {
+                                stockid = tb.Rows[0][2].ToString();
+                                txtcost.Text = tb.Rows[0][0].ToString();
+                                txtsell.Text = tb.Rows[0][1].ToString();
+
+                            }
+                            else
+                            {
+                                lblitem.Text = "--Click Here To Choose Item--";
+                                stockid = "";
+                                txtitemcode.Clear();
+                                txtdesciption.Clear();
+                                txtcategory.ResetText();
+                                txtcost.Clear();
+                                txtalert.Clear();
+                                txtsell.Clear();
+                                lblitem.Focus();
+
+                            }
+                        }
+                        else
+                        {
+                            lblitem.Text = "--Click Here To Choose Item--";
+                            stockid = "";
+                            txtitemcode.Clear();
+                            txtdesciption.Clear();
+                            txtcategory.ResetText();
+                            txtcost.Clear();
+                            txtalert.Clear();
+                            txtsell.Clear();
+                            lblitem.Focus();
+                        }
+                    }
+                    else
+                    {
+                        txtitemcode.Text = tb.Rows[0][0].ToString();
+                        txtdesciption.Text = tb.Rows[0][1].ToString();
+                        txtalert.Text = tb.Rows[0][4].ToString();
+                        txtcategory.Text = tb.Rows[0][5].ToString();
+                        txtcost.Text = tb.Rows[0][2].ToString();
+                        txtsell.Text = tb.Rows[0][3].ToString();
+
+                    }
+                    lblitem.Text ="SELECTED: "+ txtitemcode.Text + " | " + txtdesciption.Text + " | " + txtsell.Text;
+
+                }
+            }
+            else
+            {
+                lblitem.Text = "--Click Here To Choose Item--";
+                stockid = "";
+                txtitemcode.Clear();
+                txtdesciption.Clear();
+                txtcategory.ResetText();
+                txtcost.Clear();
+                txtalert.Clear();
+                txtsell.Clear();
+                lblitem.Focus();
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (stockid!="" && txtitemcode.Text!="" && txtdesciption.Text!="" && txtcost.Text!=""&&txtsell.Text!="" && txtalert.Text!="" && txtcategory.Text!="")
+            {
+                query = "update Inventory SET ItemCode='" + txtitemcode.Text + "', Cost='" + txtcost.Text + "', SellPrice='" + txtsell.Text + "', Alert='" + txtalert.Text + "', Category='" + txtcategory.Text + "' where ID='" + stockid + "'";
+                msg = db.Update(query);
+                MessageBox.Show(msg);
+                if (msg=="1")
+                {
+                    query = "update ItemInformation SET ItemCode='" + txtitemcode.Text + "', Description='" + txtdesciption.Text + "' where ItemCode='" + txtitemcode.Text + "'";
+                    msg = db.Update(query);
+                
+                    
+                }
+
+                if (msg=="1")
+                {
+                    MessageBox.Show("--Information have been successfully updated--", "Confirmation", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    txtalert.Clear();
+                    txtcategory.ResetText();
+                    txtcost.Clear();
+                    txtdesciption.Clear();
+                    txtitemcode.Clear();
+                    txtsell.Clear();
+                    lblitem.Text = "--Click Here To Choose Item--";
+                    lblitem.Focus();
+                }
+                else
+                {
+                    MessageBox.Show(msg, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            else
+            {
+                MessageBox.Show("--Invalid Entry--", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void label7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void LoadCategory()
+        {
+            query = "Select category from Inventory group by category";
+            tb = db.Search(query);
+            if (tb.Rows.Count > 0)
+            {
+                for (int i = 0; i < tb.Rows.Count; i++)
+                {
+                    txtcategory.Items.Add(tb.Rows[i][0].ToString());
+                }
+            }
+        }
+    }
+}
