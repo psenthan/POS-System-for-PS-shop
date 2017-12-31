e1ebf3d6a283cc74b7d669b70edbf3c81f6f325f
psenthan <senthanprasanth007@gmail.com>
2017-12-31 23:22:11 +0530

    Test chart implementation

---
+++ Point-Of-Scale Project /Project /Project Files (POS) /Standalone/PS Super Market/Test Chart.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using LiveCharts.Wpf;
+using LiveCharts;
+using System.Media;
+using System.Windows.Media;
+
+namespace PS_Super_Market
+{
+    public partial class Test_Chart : Form
+    {
+        public Test_Chart()
+        {
+            InitializeComponent();
+            pieChart1.InnerRadius = 100;
+            pieChart1.LegendLocation = LegendLocation.Right;
+
+            pieChart1.Series = new SeriesCollection
+            {
+                new PieSeries
+                {
+                    Title = "Chrome",
+                    Values = new ChartValues<double> {8},
+                    PushOut = 15,
+                    DataLabels = true
+                },
+                new PieSeries
+                {
+                    Title = "Mozilla",
+                    Values = new ChartValues<double> {6},
+                    DataLabels = true
+                },
+                new PieSeries
+                {
+                    Title = "Opera",
+                    Values = new ChartValues<double> {10},
+                    DataLabels = true
+                },
+                new PieSeries
+                {
+                    Title = "Explorer",
+                    Values = new ChartValues<double> {4},
+                    DataLabels = true
+                }
+            };
+
+        }
+         
+        private void Test_Chart_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
