a07a09f5dd484f9a4101e4bba8d8a65d592c4433
psenthan <senthanprasanth007@gmail.com>
2018-01-02 19:03:08 +0530

    Summary report implementation

---
+++ Point-Of-Scale Project /Project /Project Files (POS) /Standalone/PS Super Market/Summary Reports Information.Designer.cs
@@ -0,0 +1,149 @@
+﻿namespace PS_Super_Market
+{
+    partial class Summary_Reports_Information
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtfrom = new System.Windows.Forms.DateTimePicker();
+            this.txtto = new System.Windows.Forms.DateTimePicker();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.materialRaisedButton1 = new MaterialSkin.Controls.MaterialRaisedButton();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel1.BackColor = System.Drawing.Color.BlueViolet;
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(-77, -5);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(562, 50);
+            this.panel1.TabIndex = 72;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial Narrow", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(150, 14);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(266, 25);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Summary Information Reports";
+            // 
+            // txtfrom
+            // 
+            this.txtfrom.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.txtfrom.Location = new System.Drawing.Point(97, 120);
+            this.txtfrom.Name = "txtfrom";
+            this.txtfrom.Size = new System.Drawing.Size(109, 20);
+            this.txtfrom.TabIndex = 73;
+            this.txtfrom.ValueChanged += new System.EventHandler(this.txtfrom_ValueChanged);
+            // 
+            // txtto
+            // 
+            this.txtto.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.txtto.Location = new System.Drawing.Point(212, 120);
+            this.txtto.Name = "txtto";
+            this.txtto.Size = new System.Drawing.Size(109, 20);
+            this.txtto.TabIndex = 74;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(94, 104);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(82, 13);
+            this.label2.TabIndex = 75;
+            this.label2.Text = "Starting Date";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(244, 104);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(77, 13);
+            this.label3.TabIndex = 75;
+            this.label3.Text = "Ending Date";
+            // 
+            // materialRaisedButton1
+            // 
+            this.materialRaisedButton1.AutoSize = true;
+            this.materialRaisedButton1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.materialRaisedButton1.Depth = 0;
+            this.materialRaisedButton1.Icon = null;
+            this.materialRaisedButton1.Location = new System.Drawing.Point(152, 146);
+            this.materialRaisedButton1.MouseState = MaterialSkin.MouseState.HOVER;
+            this.materialRaisedButton1.Name = "materialRaisedButton1";
+            this.materialRaisedButton1.Primary = true;
+            this.materialRaisedButton1.Size = new System.Drawing.Size(108, 36);
+            this.materialRaisedButton1.TabIndex = 76;
+            this.materialRaisedButton1.Text = "View Report";
+            this.materialRaisedButton1.UseVisualStyleBackColor = true;
+            this.materialRaisedButton1.Click += new System.EventHandler(this.materialRaisedButton1_Click);
+            // 
+            // Summary_Reports_Information
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(414, 219);
+            this.Controls.Add(this.materialRaisedButton1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtto);
+            this.Controls.Add(this.txtfrom);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Summary_Reports_Information";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Load += new System.EventHandler(this.Summary_Reports_Information_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DateTimePicker txtfrom;
+        private System.Windows.Forms.DateTimePicker txtto;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private MaterialSkin.Controls.MaterialRaisedButton materialRaisedButton1;
+    }
+}
---
+++ Point-Of-Scale Project /Project /Project Files (POS) /Standalone/PS Super Market/Summary Reports Information.cs
@@ -0,0 +1,224 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using CrystalDecisions.CrystalReports.Engine;
+using CrystalDecisions.Shared;
+using System.IO;
+using System.Diagnostics;
+namespace PS_Super_Market
+{
+    public partial class Summary_Reports_Information : Form
+    {
+        public Summary_Reports_Information()
+        {
+            InitializeComponent();
+        }
+        public string Daily
+        {
+            set
+            {
+                if (value=="1")
+                {
+                    txtto.Enabled = false;
+                    label1.Text = "Daily Summary Report";
+                }
+                else
+                {
+                    txtto.Enabled = true;
+                  
+                }
+            }
+        }
+        public string Info
+        {
+            set
+            {
+                if (value == "1")
+                {
+                    txtto.Enabled = false;
+                }
+                else
+                {
+                    txtto.Enabled = true;
+                }
+            }
+        }
+        private void Summary_Reports_Information_Load(object sender, EventArgs e)
+        {
+
+        }
+        MainClass db = new MainClass();
+        DataTable tb;
+        string query, msg;
+        double Sales, Purchasing, Expenses, Deposit, Withdraw, openingbalance, closingbalance, profit, Return;
+
+        private void txtfrom_ValueChanged(object sender, EventArgs e)
+        {
+            if (txtto.Enabled == false)
+            {
+                txtto.Text = txtfrom.Text;
+            }
+        }
+
+        private void materialRaisedButton1_Click(object sender, EventArgs e)
+        {
+            {
+
+                try
+                {
+                    this.Cursor = Cursors.WaitCursor;
+                    //openingbalance
+
+                    query = "select Start from Shift where Date='" + txtfrom.Text + "'";
+                    tb = db.Search(query);
+                    if (tb.Rows.Count > 0)
+                    {
+                        openingbalance = double.Parse(tb.Rows[0][0].ToString());
+                    }
+                    else
+                    {
+                        openingbalance = 0;
+                    }
+
+                    //openingbalance
+
+                    query = "select [End] from Shift where Date='" + txtto.Text + "'";
+                    tb = db.Search(query);
+                    if (tb.Rows.Count > 0)
+                    {
+                        try
+                        {
+                            closingbalance = double.Parse(tb.Rows[0][0].ToString());
+                        }
+                        catch
+                        {
+
+                            closingbalance = 0;
+                        }
+                    }
+                    else
+                    {
+                        closingbalance = 0;
+                    }
+
+                    //Sales Total
+                    query = "select sum(SubTotal) from Billing where Date>='" + txtfrom.Text + "' and Date<='" + txtto.Text + "' and Subtotal>0 ";
+                    tb = db.Search(query);
+                    if (tb.Rows[0][0].ToString() != "")
+                    {
+                        Sales = double.Parse(tb.Rows[0][0].ToString());
+                    }
+                    else
+                    {
+                        Sales = 0;
+                    }
+
+                    //return
+                    query = "select sum(SubTotal) from Billing where Date>='" + txtfrom.Text + "' and Date<='" + txtto.Text + "' and Subtotal<0 ";
+                    tb = db.Search(query);
+                    if (tb.Rows[0][0].ToString() != "")
+                    {
+                        Return = double.Parse(tb.Rows[0][0].ToString());
+                    }
+                    else
+                    {
+                        Return = 0;
+                    }
+                    //Purchasing
+                    query = "select sum(SubTotal) from Purchasing where Date>='" + txtfrom.Text + "' and Date<='" + txtto.Text + "'";
+                    tb = db.Search(query);
+                    if (tb.Rows[0][0].ToString() != "")
+                    {
+                        Purchasing = double.Parse(tb.Rows[0][0].ToString());
+                    }
+                    else
+                    {
+                        Purchasing = 0;
+                    }
+
+                    //Expenses
+                    query = "select sum(Amount) from Expenses where Date>='" + txtfrom.Text + "' and Date<='" + txtto.Text + "'";
+                    tb = db.Search(query);
+                    if (tb.Rows[0][0].ToString() != "")
+                    {
+                        Expenses = double.Parse(tb.Rows[0][0].ToString());
+                    }
+                    else
+                    {
+                        Expenses = 0;
+                    }
+
+                    //Bank Deposit
+                    query = "select sum(Amount) from BankTransactions where Type='" + "Deposit" + "' and Date>='" + txtfrom.Text + "' and Date<='" + txtto.Text + "'";
+                    tb = db.Search(query);
+                    if (tb.Rows[0][0].ToString() != "")
+                    {
+                        Deposit = double.Parse(tb.Rows[0][0].ToString());
+                    }
+                    else
+                    {
+                        Deposit = 0;
+                    }
+
+
+                    //Withdraw
+                    query = "select sum(Amount) from BankTransactions where Type='" + "Withdraw" + "' and Date>='" + txtfrom.Text + "' and Date<='" + txtto.Text + "'";
+                    tb = db.Search(query);
+                    if (tb.Rows[0][0].ToString() != "")
+                    {
+                        Withdraw = double.Parse(tb.Rows[0][0].ToString());
+                    }
+                    else
+                    {
+                        Withdraw = 0;
+                    }
+
+                    //Profit
+                    query = "select   sum((Billing.SellPrice-Inventory.Cost)*Billing.Quantity)  from Inventory, Billing, ItemInformation where ItemInformation.ItemCode=Inventory.ItemCode and Inventory.ID=Billing.ItemID  and Billing.Date>='" + txtfrom.Text + "' and Billing.Date<='" + txtto.Text + "'";
+                    tb = db.Search(query);
+                    if (tb.Rows[0][0].ToString() != "")
+                    {
+                        profit = double.Parse(tb.Rows[0][0].ToString());
+                    }
+                    else
+                    {
+                        profit = 0;
+                    }
+                    ReportDocument rep = new ReportDocument();
+                    rep.Load(@"C:\PS Supermarket\Summary Report.rpt");
+                    rep.SetParameterValue("From", txtfrom.Text);
+                    rep.SetParameterValue("To", txtto.Text);
+                    rep.SetParameterValue("Sales", Sales);
+                    rep.SetParameterValue("Returns", Return);
+                    rep.SetParameterValue("Purchasing", Purchasing);
+                    rep.SetParameterValue("Expenses", Expenses);
+                    rep.SetParameterValue("Deposit", Deposit);
+                    rep.SetParameterValue("Withdraw", Withdraw);
+                    rep.SetParameterValue("Profit", profit);
+                    rep.SetParameterValue("OpeningBalance", openingbalance);
+                    rep.SetParameterValue("ClosingBalance", closingbalance);
+                    string path = @"C:\PS Supermarket\Summary Report.pdf";
+                    db.CrystalReportServer(rep);
+                    if (File.Exists(path))
+                    {
+                        File.Delete(path);
+                    }
+                    rep.ExportToDisk(ExportFormatType.PortableDocFormat, path);
+                    Process.Start(path);
+                    this.Cursor = Cursors.Default;
+                }
+                catch (Exception ex)
+                {
+                    this.Cursor = Cursors.Default;
+                    MessageBox.Show(ex.Message.ToString(), "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+    }
+}
