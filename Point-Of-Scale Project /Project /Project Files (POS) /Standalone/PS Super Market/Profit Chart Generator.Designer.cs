75008440df98207fde0fec1bbd4088efca0934a9
psenthan <senthanprasanth007@gmail.com>
2018-01-03 22:56:07 +0530

    Profit chart implementation

---
+++ Point-Of-Scale Project /Project /Project Files (POS) /Standalone/PS Super Market/Profit Chart Generator.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace PS_Super_Market
+{
+    partial class Profit_Chart_Generator
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtyear = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.BlueViolet;
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(437, 50);
+            this.panel1.TabIndex = 71;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial Narrow", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(107, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(199, 25);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Profit Chart Generator";
+            // 
+            // txtyear
+            // 
+            this.txtyear.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtyear.FormattingEnabled = true;
+            this.txtyear.Location = new System.Drawing.Point(112, 118);
+            this.txtyear.Name = "txtyear";
+            this.txtyear.Size = new System.Drawing.Size(194, 33);
+            this.txtyear.TabIndex = 72;
+            this.txtyear.SelectedIndexChanged += new System.EventHandler(this.txtyear_SelectedIndexChanged);
+            this.txtyear.Leave += new System.EventHandler(this.txtyear_Leave);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(108, 95);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(113, 20);
+            this.label2.TabIndex = 73;
+            this.label2.Text = "Choose Year";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(143, 157);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(131, 38);
+            this.button1.TabIndex = 74;
+            this.button1.Text = "View Report As PDF";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Profit_Chart_Generator
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(437, 225);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtyear);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Profit_Chart_Generator";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Load += new System.EventHandler(this.Profit_Chart_Generator_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox txtyear;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+    }
+}
---
+++ Point-Of-Scale Project /Project /Project Files (POS) /Standalone/PS Super Market/Profit Chart Generator.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using CrystalDecisions.CrystalReports.Engine;
+using CrystalDecisions.Shared;
+using System.IO;
+using System.Diagnostics;
+using LiveCharts.Wpf;
+using LiveCharts;
+namespace PS_Super_Market
+{
+    public partial class Profit_Chart_Generator : Form
+    {
+        public Profit_Chart_Generator()
+        {
+            InitializeComponent();
+        }
+        MainClass db = new MainClass();
+        DataTable tb;
+        string query, msg;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (txtyear.Text != "")
+            {
+                try
+                {
+                    
+                    this.Cursor = Cursors.WaitCursor;
+                    db.Delete("delete from TempSalesChart");
+                    query = "select   Month( Billing.Date), sum((Billing.SellPrice-Inventory.Cost)*Billing.Quantity)  from Inventory, Billing, ItemInformation where Iteminformation.ItemCode=Inventory.ItemCode and Inventory.ID=Billing.ItemID and Year(Billing.Date)='" + txtyear.Text + "'   group by Month(Billing.Date) ";
+                    tb = db.Search(query);
+                    if (tb.Rows.Count > 0)
+                    {
+                        for (int i = 0; i < tb.Rows.Count; i++)
+                        {
+                            query = "insert into TempSalesChart values ('" + txtyear.Text + "', '" + tb.Rows[i][0].ToString() + "','" + tb.Rows[i][1].ToString() + "')";
+                            db.Insert(query);
+                        }
+
+                        ReportDocument rep = new ReportDocument();
+                        rep.Load(@"C:\PS Supermarket\Profit Chart Report.rpt");
+                        db.CrystalReportServer(rep);
+                        string path = @"C:\PS Supermarket\Profit Chart.pdf";
+                        if (File.Exists(path))
+                        {
+                            File.Delete(query);
+                        }
+                        rep.ExportToDisk(ExportFormatType.PortableDocFormat, path);
+                        Process.Start(path);
+                    }
+                    else
+                    {
+                        MessageBox.Show("--No Records Found--", "System Admin", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    }
+                    this.Cursor = Cursors.Default;
+                }
+                catch (Exception ex)
+                {
+                    this.Cursor = Cursors.Default;
+                    MessageBox.Show(ex.Message.ToString(), "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Invalid Entry", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void txtyear_Leave(object sender, EventArgs e)
+        {
+            if (CheckIsInt(txtyear.Text) && txtyear.Text.Length == 4)
+            {
+
+            }
+            else
+            {
+                MessageBox.Show("Invalid Entry", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                txtyear.ResetText();
+                txtyear.Focus();
+            }
+        }
+        private bool CheckIsInt(string input)
+        {
+            int value;
+            if (int.TryParse(input, out value))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        private void txtyear_SelectedIndexChanged(object sender, EventArgs e)
+        {
+             
+           
+        }
+
+        private void Profit_Chart_Generator_Load(object sender, EventArgs e)
+        {
+            query = "select Year(Date) from Billing group by Year(Date)";
+            tb = db.Search(query);
+            if (tb.Rows.Count > 0)
+            {
+                for (int i = 0; i < tb.Rows.Count; i++)
+                {
+                    txtyear.Items.Add(tb.Rows[i][0].ToString());
+                }
+            }
+        }
+    }
+}
